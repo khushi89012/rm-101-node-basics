1d708f11ce0de633b82adc9e93457396822b120e
khushi Gupta <95909861+khushi89012@users.noreply.github.com>
2022-06-20 20:15:26 +0530

    completed

--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
-// install and import express
-const express = () => {};
+
+
+
+
+// const express = () => {};
+const express = require('express');
+
 let app = express();
 
-// Code here
 
-// Note: Do not remove this export statement
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "./assets/users.html");
+}).get("/users", (req, res) => {
+    res.sendFile(__dirname + "./assets/user.json");
+}).get("/users/:id", (req, res) => {
+    res.sendFile(__dirname + "./assets/user.json");
+}).post("/users", (req, res) => {
+    res.sendFile(__dirname + "./assets/user.json");
+}).listen(8000);
+
+
+
 module.exports = app;
+
+
